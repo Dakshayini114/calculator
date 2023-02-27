3230a8e4a16a80a3e5ca84a42a784576b6ab35c2
Dakshayini114 <pataparladakshayini3@gmail.com>
2023-02-27 15:40:30 -0500

    hi

---
+++ calculator.py
@@ -0,0 +1,10 @@
+x=int(input("enter your choice"))
+print("enter two numbers")
+num1=int(input())
+num2=int(input())
+if x==1:
+    r=num1+num2
+    print("result",r)
+elif x==2:
+    r=num1-num2
+    print("result",r)
