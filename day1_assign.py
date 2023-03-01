fd56b873535ed0ec69de5356bb8bcc1de43cd2d4
Dakshayini114 <pataparladakshayini3@gmail.com>
2023-03-01 09:37:16 -0500

    this is my Day-1 assignment

---
+++ day1_assign.py
@@ -0,0 +1,45 @@
+# Try to print “HelloWorld “ by adding statement “print(“Hello World”) in hello.py file#
+
+#print("hello world")
+
+#Try to create variable as X and store your name inside it#
+
+#x="dakshayini"
+#print(x)
+
+#Try to create a list of fruits and append a new fruit after initial creation
+
+#fruits=["apple","banana","mango"]
+#fruits.append("orange")
+#print(fruits)
+
+#Try to create a tuple of dishes and think of updating the list
+
+#x=("rice","chicken")
+#print(x)
+#y=("sambar")
+#print(y)
+
+#Try to create a dictionary of name, class and grade of a fictional class. Add atleast 5
+#member names.
+
+#x={}
+#x["name"]="daksha","anil","shukla","faheema"
+#x["class"]=3,4,5,6
+#x["grade"]=9.3,9.4,9.5,9.6
+#print(x)
+#x["name"]="abc"
+
+x={"name":"daksha","class":3, "grade":9.3}
+print(x)
+
+#list examples
+
+#x=["a","b","c"]
+#x[1]="d"
+#print(x)
+
+#x=["a","b","c"]
+#print(x[0:1])
+
+
