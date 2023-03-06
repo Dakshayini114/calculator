3366ec6350b981bfe22b456f3dce8cf19544cac6
Dakshayini114 <pataparladakshayini3@gmail.com>
2023-03-06 12:54:05 -0500

    this is my Day-2 assignment

---
+++ day2_assign.py
@@ -0,0 +1,84 @@
+#using if and else
+#x=10
+#if x>5:
+#   print("x")
+#else:
+#  print("y")
+
+#using if, elseif and else
+#x=10
+#if x>10:
+ #   print("x")
+#elif x==10:
+ #   print("y")
+#else:
+ #   print("z")
+
+#using nestedif
+
+#x=10
+#if x>5:
+ #   print("abc")
+  #  if x<10:
+  #      print("x")
+  #  elif x==10:
+  #      print("y")
+   # else:
+   #     print("z")
+
+#using while
+
+#i=10
+#while i<20:
+ #   print(i)
+#    i+=1
+
+#i=10
+#while i<20:
+ #   print(i)
+ #   i+=1
+#else:
+ #   print("good")
+
+#using for
+
+#i=(10,20,30)
+#for x in i:
+ #   print(x)
+
+#i="anil"
+#for x in i:
+ #   print(x)
+
+
+#x=["anil","abc","bcd"]
+#if "anil" in x:
+#     print("true")
+
+#x=["anil","abc","bcd"]
+#for i in x:
+ #   if i=="anil":
+  #      print(i)
+
+#x=[10,20,30,40,50]
+#temp=0
+##for i in x:
+  #  temp=i+temp
+ #   print(temp)
+
+
+x=10
+if x==20 and x==10:
+    print("yes")
+elif x==20 or x==10:
+    print("yessss")
+#x=int(input("enter your choice"))
+#print("enter two numbers")
+#num1=int(input())
+#num2=int(input())
+#if x==1:
+ #   r=num1+num2
+ #   print("result",r)
+#elif x==2:
+  #  r=num1-num2
+  #  print("result",r)
